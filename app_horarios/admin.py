0d351ef0a87074a1ac8142e5f6b9a787488d26a9
Eduardo Salinas <eduardoluis93@gmail.com>
2023-01-02 18:06:41 +0000

    Cree La Tabla de Secretarios

--- app_horarios/admin.py
+++ app_horarios/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Esto importará todos los modelos que he creado
-from .models import User, Chofer
+from .models import User, Chofer, Secretario
 
 # Registra tus modelos aquí
 
 # Esto registra cada modelo de models.py. Deberia ponermelo visible en el panel de admin de Django
 admin.site.register(User)
 admin.site.register(Chofer)
+admin.site.register(Secretario)
--- app_horarios/models.py
+++ app_horarios/models.py
@@ -43,3 +43,34 @@ class Chofer(models.Model):
     telefono = models.CharField(blank=True, max_length=20)
     email = models.EmailField(blank=True, max_length=254)
 
+
+""" Modelo de Secretarios/as 	
+
+Lo que voy a hacer es una tabla llamada “secretarios”, en los cuales meteré a algunos usuarios y les daré el status de 
+“secretario” con un booleano. Si son secretarios, podrán ver las páginas con los datos de los choferes y los reportes, 
+A PESAR DE que no puedan entrar en el panel del admin. 
+
+En el modelo de “Secretario”, pondré que usuario que no sea super usuario pueda ver los reportes semanales y otros 
+datos. Pondré la ID del usuario a través de una clave foránea, su nombre de usuario (clave foránea del usuario), y si 
+el usuario es secretario o no (mediante un booleano).
+
+Si el usuario es secretario (el booleano es “True”), ellos, junto con los súper usuarios, podrán ver los reportes 
+semanales y otros datos privados de la web app.
+
+Los datos que necesito para la tabla “Secretario” son:
+•	ID del usuario (tomada como clave foránea de User)
+•	Si el usuario es secretario o no (booleano)
+
+Si el usuario ha sido agregado a esta tabla, entonces siempre será un secretario. Entonces, en el campo que pregunta
+si es secretario o no SIEMPRE se debe dejar en "True".
+
+Como le puse “True” como valor por defecto, el administrador no tendrá que tocar nada cuando seleccione al usuario para 
+convertirlo en secretario. Solo tendrá que escoger la ID del usuario al que quiere convertir en secretario.
+"""
+class Secretario(models.Model):
+
+    # ID del chofer (tomado como clave foranea)
+    id_de_usuario = models.ForeignKey("User", on_delete=models.CASCADE, related_name="id_de_usuario_de_secretario")
+
+    # Si el usuario es secretario (SIEMPRE DEJARLO EN "True")
+    el_usuario_es_secretario = models.BooleanField(default=True)
