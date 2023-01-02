9249b9ab673e74b12fd2e2225a45067d43ef4ab6
Eduardo Salinas <eduardoluis93@gmail.com>
2023-01-02 15:11:40 +0000

    Agregue Modelos para que se vean en el Admin

--- app_horarios/admin.py
+++ app_horarios/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+# Esto importará todos los modelos que he creado
+from .models import User, Chofer
+
+# Registra tus modelos aquí
+
+# Esto registra cada modelo de models.py. Deberia ponermelo visible en el panel de admin de Django
+admin.site.register(User)
+admin.site.register(Chofer)
