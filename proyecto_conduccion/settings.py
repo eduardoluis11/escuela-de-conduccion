d7d98b897546833e930f1f71df63f24fe9f1828b
Eduardo Salinas <eduardoluis93@gmail.com>
2023-01-14 17:48:05 +0000

    Hid Django Key Into Environment Variable, and created requirements.txt

--- proyecto_conduccion/settings.py
+++ proyecto_conduccion/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import pathlib
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v(37*%x+r2zyy&$v3@oq1_a8x%%z07299p=5&n-fyimqzw_)q3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
