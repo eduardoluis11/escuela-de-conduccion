f104e3fe9257e03eb48a246312a6a6b8d3ecfcaa
Eduardo Salinas <eduardoluis93@gmail.com>
2023-01-15 00:00:15 +0000

    Volvi a poner la clave de Django en settings.py

--- proyecto_conduccion/settings.py
+++ proyecto_conduccion/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import pathlib
 from pathlib import Path
 
-from decouple import config
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-v(37*%x+r2zyy&$v3@oq1_a8x%%z07299p=5&n-fyimqzw_)q3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
